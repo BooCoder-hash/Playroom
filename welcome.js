d2b464676c1267669b76f0bb6159f395c00e1025
BooCoder-hash <80242633+BooCoder-hash@users.noreply.github.com>
2021-03-23 06:56:20 +0000

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,17 @@
+module.exorts = (client) => {
+   const channelId = '797683010115600395' // welcome channel
+   const targetChannelId = '812818387276791888' // rules and info
+
+   client.on('guildMemberAdd', member => {
+     console.log(member)
+
+     const message = `Welcome <@${
+            memberid
+    }> to the server Dont forget to check out ${member.guild.channels.cache
+     .get(targetChannelId)
+     .toString()}`
+
+    const channel = member.guild.channels.cache.get(channelId)
+    channel.send(message)
+   })
+}
