60cdb7277eb3bd96c5669e594c593c88249fd543
BooCoder-hash <80242633+BooCoder-hash@users.noreply.github.com>
2021-03-23 07:01:39 +0000

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,17 +1,16 @@
 module.exorts = (client) => {
-   const channelId = '797683010115600395' // welcome channel
-   const targetChannelId = '812818387276791888' // rules and info
+    const channelId = '797683010115600395' // welcome channel
+    const targetChannelId = '812818387276791888' // rules and info
 
-   client.on('guildMemberAdd', member => {
-     console.log(member)
+    client.on('guildMemberAdd', member => {
+        console.log(member)
 
-     const message = `Welcome <@${
-            memberid
-    }> to the server Dont forget to check out ${member.guild.channels.cache
-     .get(targetChannelId)
-     .toString()}`
+        const message = `Welcome <@${memberid
+            }> to the server Dont forget to check out ${member.guild.channels.cache
+                .get(targetChannelId)
+                .toString()}`
 
-    const channel = member.guild.channels.cache.get(channelId)
-    channel.send(message)
-   })
+        const channel = member.guild.channels.cache.get(channelId)
+        channel.send(message)
+    })
 }
